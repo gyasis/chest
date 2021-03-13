f999d7973e2f151de2aa85c977a589c18d84785c
Gyasi Sutton <gyasis@gmail.com>
2021-03-13 16:11:38 +0100

    photogrid

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ from pydicom.data import get_testdata_file
 
 # %%
 # The path to a pydicom test dataset
-path = 'chest/data/009bc039326338823ca3aa84381f17f1.dicom'
+path = 'data/small_collection/009bc039326338823ca3aa84381f17f1.dicom'
 ds = dcmread(path)
 # `arr` is a numpy.ndarray
 arr = ds.pixel_array
