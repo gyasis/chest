993f682de5f78ee26e28f74ad0316369ebcb08e0
gyasis <gyasis@gmail.com>
2021-07-08 11:16:26 -0400

    working model and experiment, approx 8 hours running time

--- imgshow.py
+++ imgshow.py
@@ -4,6 +4,7 @@
 import glob
 radimg = glob.glob('data/small_collection/*.dicom')
 print(radimg)
+# %%
 import re
 imagelist = []
 for item in radimg:
@@ -61,20 +62,20 @@ plt.imshow(arr, cmap="gray")
 
 
 # %%
-def interest_area():
+# def interest_area():
     
-    # later get index for coordinates
-    # Create a Rectangle patch
-    rect = patches.Rectangle((50, 100), 40, 30, linewidth=1, edgecolor='r', facecolor='none')
+#     # later get index for coordinates
+#     # Create a Rectangle patch
+#     rect = patches.Rectangle((50, 100), 40, 30, linewidth=1, edgecolor='r', facecolor='none')
 
-    # Add the patch to the Axes
-    return fig2.add_patch(rect)
+#     # Add the patch to the Axes
+#     return fig2.add_patch(rect)
 
-fig2 = plt.figure(figsize=(30,60))
-interest_area()
+# fig2 = plt.figure(figsize=(30,60))
+# interest_area()
 
 
-plt.imshow(arr, cmap='gray')
+# plt.imshow(arr, cmap='gray')
 
 # %%
 contents.Rows
@@ -89,3 +90,69 @@ import pandas as pd
 
 df = pd.read_csv('data/train.csv')
 df.head(10)
+
+# %%
+from torch.utils.data import DataLoader, Dataset, random_split
+
+# %%
+df = df[['image_id', 'class_id']]
+# %%
+def build_path(x):
+    path_ = '/media/gyasis/Drive 2/Data/vinbigdata/train/'
+    filetype = '.dicom'
+    x = (path_+x+filetype)
+    return x
+# %%
+import os.path
+# %%
+df['imagepath'] = df['image_id'].apply(lambda x: build_path(x))
+
+# %%
+#os.path.exists(df.imagepath[0])
+
+# %%
+df = df[['imagepath','class_id']]
+# %%
+df.head()
+# %%
+
+
+# %%
+contents = dcmread(df.imagepath[0])
+print(contents)
+# %%
+fig2 = plt.figure(figsize=(30,60))
+arr = contents.pixel_array
+plt.imshow(arr, cmap="gray")
+
+
+# %%
+class MyDataset(Dataset):
+    def __init__(self, dataset, transform=None):
+        print('creating Dataset')
+        self.df = dataset
+        self.imagearray = np.asarray(dataset.imagepath)
+        self.class_arr = np.asarray(dataset.class_id)
+        self.transform = transform
+        self.data_len = len(dataset.index)
+        
+    def __getitem__(self, index):
+        ds = dcmread(self.imagearray[index])
+        arr = ds.pixel_array
+        class_id = self.df.loc[index, 'class_id']
+        return arr , class_id
+        
+    def __len__(self):
+        return self.data_len
+
+# %%
+chestdata = MyDataset(df)
+# %%
+for i in range (4):
+    # print(chestdata[i])
+    print(chestdata[i][0])
+    plt.imshow(chestdata[i][0], cmap="gray")
+# %%
+
+# %%
+# %%
